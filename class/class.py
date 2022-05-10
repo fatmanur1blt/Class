24bac870faaae7d4b0e5c41dd52ad01ea412a63c
Fatmanur BULUT <49518264+fatmanur1blt@users.noreply.github.com>
2022-05-11 00:16:58 +0300

    Add files via upload

---
+++ class/class.py
@@ -0,0 +1,14 @@
+class Ucus():
+    havayolu = 'FNB'
+
+    def __init__(self, kod, kalkis, varis, sure, kapasite, yolcu):
+        self.kod = kod
+        self.kalkis = kalkis
+        self.varis = varis
+        self.sure = sure
+        self.kapasite = kapasite
+        self.yolcu = yolcu
+        ucus2 = Ucus('FB223', 'Istanbul', 'Berlin', 180 ,300, 250)
+        ucus2.kod
+
+
